78b3dfb86dcf6eae5e895317d6f5abaeea070702
sujit995 <sujitmishra402@gmail.com>
2021-11-12 10:06:11 +0530

    adding projects

--- src/Components/allportfolios.js
+++ src/Components/allportfolios.js
@@ -2,68 +2,58 @@ import port1 from '../img/portImages/port1.png';
 import port2 from '../img/portImages/port2.png';
 import port3 from '../img/portImages/port3.png';
 import port4 from '../img/portImages/port4.png';
-// import port5 from '../img/portImages/port5.jpg';
+import port5 from '../img/portImages/port5.png';
 
 const portfolios = [
     {
         id: 1,
         category: 'Full Stack',
-        link1: 'https://sujit995-social-memories-app.netlify.app',
-        link2: 'https://github.com/sujit995/mern-memories-app-frontend',
+        link1: 'https://sujit995-onlinefood-delivery.netlify.app',
+        link2: 'https://github.com/sujit995/food-order-app',
         icon1: 'Live',
         icon2: 'Source',
         image: port1,
-        title: 'Memories App',
+        title: 'Online Food Ordering App',
     },
     {
         id: 2,
+        category: 'Full Stack',
+        link1: 'https://sujit995-photo-gallery.netlify.app',
+        link2: 'https://github.com/sujit995/photo-gallery-app',
+        icon1: 'Live',
+        icon2: 'Source',
+        image: port2,
+        title: 'Photo Gallery App'
+    },
+    {
+        id: 3,
         category: 'React Js',
         link1: 'https://sujit995-movie-app.netlify.app',
         link2: 'https://github.com/sujit995/movie-details-app',
         icon1: 'Live',
         icon2: 'Source',
-        image: port2,
+        image: port3,
         title: 'Movie Details App'
     },
     {
-        id: 3,
-        category: 'React Js',
-        link1: 'https://sujit-portfolio.netlify.app',
-        link2: 'https://github.com/sujit995/myreactportfolio',
-        icon1: 'Live',
-        icon2: 'Source',
-        image: port3,
-        title: 'Portfolio'
-    },
-    {
         id: 4,
-        category: 'Full Stack',
-        link1: 'https://smartbrain-face-detectapp.herokuapp.com/',
-        link2: 'https://github.com/sujit995/face_recognition_app',
+        category: 'React Js',
+        link1: 'https://sujit995-petshop.netlify.app',
+        link2: 'https://github.com/sujit995/pet-shop-website',
         icon1: 'Live',
         icon2: 'Source',
         image: port4,
-        title: 'Smart Brain Face Detect App'
+        title: 'Pet-shop SPA'
     },
     // {
     //     id: 5,
-    //     category: 'React Js',
-    //     link1: 'www.youtube.com',
-    //     link2: 'www.github.com',
-    //     icon1: 'G',
-    //     icon2: 'Y',
-    //     image: port6,
-    //     title: 'Getting Started With React'
-    // },
-    // {
-    //     id: 6,
-    //     category: 'React Js',
-    //     link1: 'www.youtube.com',
-    //     link2: 'www.github.com',
-    //     icon1: 'G',
-    //     icon2: 'Y',
-    //     image: port7,
-    //     title: 'Vanilla Javascript Website'
+    //     category: 'Full Stack',
+    //     link1: 'https://sujit995-social-memories-app.netlify.app',
+    //     link2: 'https://github.com/sujit995/mern-memories-app-frontend',
+    //     icon1: 'Live',
+    //     icon2: 'Source',
+    //     image: port5,
+    //     title: 'Memories App',
     // },
 ]
 
